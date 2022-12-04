9055c3f95a2667f5272b1d0076deb86ff2972b61
Matilde Park <m@park.computer>
2022-12-03 20:47:05 -0800

    extra_buttons: fix

--- src/components/extra_buttons/extra_buttons.vue
+++ src/components/extra_buttons/extra_buttons.vue
@@ -14,9 +14,9 @@
           class="button-default dropdown-item dropdown-item-icon"
           @click.prevent="muteConversation"
         >
-          <FAIcon
-            fixed-width
-            icon="eye-slash"
+          <i
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:4px;"
+            class="las la-bell-slash"
           /><span>{{ $t("status.mute_conversation") }}</span>
         </button>
         <button
@@ -24,9 +24,9 @@
           class="button-default dropdown-item dropdown-item-icon"
           @click.prevent="unmuteConversation"
         >
-          <FAIcon
-            fixed-width
-            icon="eye-slash"
+          <i
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:4px;"
+            class="las la-bell"
           /><span>{{ $t("status.unmute_conversation") }}</span>
         </button>
         <button
@@ -35,9 +35,9 @@
           @click.prevent="pinStatus"
           @click="close"
         >
-          <FAIcon
-            fixed-width
-            icon="thumbtack"
+          <i
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:4px;"
+            class="las la-thumbtack"
           /><span>{{ $t("status.pin") }}</span>
         </button>
         <button
@@ -46,9 +46,9 @@
           @click.prevent="unpinStatus"
           @click="close"
         >
-          <FAIcon
-            fixed-width
-            icon="thumbtack"
+          <i
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:4px;"
+            class="las la-thumbtack"
           /><span>{{ $t("status.unpin") }}</span>
         </button>
         <button
@@ -57,9 +57,9 @@
           @click.prevent="bookmarkStatus"
           @click="close"
         >
-          <FAIcon
-            fixed-width
-            :icon="['far', 'bookmark']"
+          <i
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:4px;"
+            class="las la-bookmark"
           /><span>{{ $t("status.bookmark") }}</span>
         </button>
         <button
@@ -68,52 +68,21 @@
           @click.prevent="unbookmarkStatus"
           @click="close"
         >
-          <FAIcon
-            fixed-width
-            icon="bookmark"
+          <i
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:4px;"
+            class="las la-bookmark"
           /><span>{{ $t("status.unbookmark") }}</span>
-        </button>
-        <button
-          v-if="ownStatus && editingAvailable"
-          class="button-default dropdown-item dropdown-item-icon"
-          @click.prevent="editStatus"
-          @click="close"
-        >
-          <FAIcon
-            fixed-width
-            icon="pen"
-          /><span>{{ $t("status.edit") }}</span>
-        </button>
-        <button
-          v-if="isEdited && editingAvailable"
-          class="button-default dropdown-item dropdown-item-icon"
-          @click.prevent="showStatusHistory"
-          @click="close"
-        >
-          <FAIcon
-            fixed-width
-            icon="history"
-          /><span>{{ $t("status.edit_history") }}</span>
-        </button>
-        <button
-          v-if="ownStatus"
-          class="button-default dropdown-item dropdown-item-icon"
-          @click.prevent="redraftStatus"
-          @click="close"
-        >
-          <FAIcon
-            fixed-width
-            icon="file-pen"
-          /><span>{{ $t("status.redraft") }}</span>
         </button>
         <button
           v-if="canDelete"
           class="button-default dropdown-item dropdown-item-icon"
+          style="display:flex;align-items:center;"
           @click.prevent="deleteStatus"
           @click="close"
         >
           <svg
-            stroke="var(--Text,#000000"
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:0px;"
+            stroke="var(--Text,#000000)"
             width="16px"
             height="16px"
           >
@@ -129,10 +98,17 @@
           @click.prevent="copyLink"
           @click="close"
         >
-          <FAIcon
-            fixed-width
-            icon="share-alt"
-          /><span>{{ $t("status.copy_link") }}</span>
+          <svg
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:0px;"
+            width="16px"
+            height="16px"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M7.78361 2.70909C8.30172 2.19097 9.00444 1.8999 9.73716 1.8999C11.263 1.8999 12.4999 3.13682 12.4999 4.66264C12.4999 5.39537 12.2088 6.09808 11.6907 6.6162L7.08483 11.2221C6.77888 11.528 6.36394 11.6999 5.93127 11.6999C5.03029 11.6999 4.2999 10.9695 4.2999 10.0685C4.2999 9.63587 4.47178 9.22092 4.77772 8.91498L9.24635 4.44635L9.95345 5.15346L5.48483 9.62209C5.36642 9.74049 5.2999 9.90108 5.2999 10.0685C5.2999 10.4172 5.58258 10.6999 5.93127 10.6999C6.09872 10.6999 6.25931 10.6334 6.37772 10.515L10.9836 5.90909C11.3142 5.57851 11.4999 5.13015 11.4999 4.66264C11.4999 3.68911 10.7107 2.8999 9.73716 2.8999C9.26965 2.8999 8.82129 3.08562 8.49071 3.4162L3.74757 8.15934C3.20482 8.7021 2.8999 9.43822 2.8999 10.2058C2.8999 11.8042 4.19564 13.0999 5.79401 13.0999C6.56158 13.0999 7.29771 12.795 7.84046 12.2522L12.4463 7.64635L13.1535 8.35346L8.54757 12.9593C7.81728 13.6896 6.8268 14.0999 5.79401 14.0999C3.64336 14.0999 1.8999 12.3564 1.8999 10.2058C1.8999 9.17301 2.31017 8.18252 3.04046 7.45224L7.78361 2.70909Z"
+            />
+          </svg><span>{{ $t("status.copy_link") }}</span>
         </button>
         <a
           v-if="!status.is_local"
@@ -141,9 +117,9 @@
           :href="status.external_url"
           target="_blank"
         >
-          <FAIcon
-            fixed-width
-            icon="external-link-alt"
+          <i
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:4px;font-size:16px;"
+            class="las la-external-link-alt"
           /><span>{{ $t("status.external_source") }}</span>
         </a>
         <button
@@ -151,31 +127,10 @@
           @click.prevent="reportStatus"
           @click="close"
         >
-          <FAIcon
-            fixed-width
-            :icon="['far', 'flag']"
+          <i
+            style="display:inline-block;vertical-align:middle;cursor:pointer;margin-left:4px;margin-right:4px;font-size:16px;"
+            class="las la-flag"
           /><span>{{ $t("user_card.report") }}</span>
-        </button>
-        <button
-          v-if="canTranslate"
-          class="button-default dropdown-item dropdown-item-icon"
-          @click.prevent="translateStatus"
-          @click="close"
-        >
-          <FAIcon
-            fixed-width
-            icon="globe"
-          /><span>{{ $t("status.translate") }}</span>
-
-          <template v-if="noTranslationTargetSet">
-            <span class="dropdown-item-icon__badge warning">
-              <FAIcon
-                fixed-width
-                icon="exclamation-triangle"
-                name="test"
-              />
-            </span>
-          </template>
         </button>
       </div>
     </template>
@@ -183,7 +138,8 @@
       <button class="button-unstyled popover-trigger">
         <svg
           slot="trigger"
-          style="display:inline-block;vertical-align:middle;cursor:pointer;"
+          class="popover-trigger"
+          style="display:inline-block;cursor:pointer;"
           color="var(--Text)"
           width="16px"
           height="16px"
@@ -195,28 +151,6 @@
           />
         </svg>
       </button>
-      <teleport to="#modal">
-        <ConfirmModal
-          v-if="showingDeleteDialog"
-          :title="$t('status.delete_confirm_title')"
-          :cancel-text="$t('status.delete_confirm_cancel_button')"
-          :confirm-text="$t('status.delete_confirm_accept_button')"
-          @cancelled="hideDeleteStatusConfirmDialog"
-          @accepted="doDeleteStatus"
-        >
-          {{ $t('status.delete_confirm') }}
-        </ConfirmModal>
-        <ConfirmModal
-          v-if="showingRedraftDialog"
-          :title="$t('status.redraft_confirm_title')"
-          :cancel-text="$t('status.redraft_confirm_cancel_button')"
-          :confirm-text="$t('status.redraft_confirm_accept_button')"
-          @cancelled="hideRedraftStatusConfirmDialog"
-          @accepted="doRedraftStatus"
-        >
-          {{ $t('status.redraft_confirm') }}
-        </ConfirmModal>
-      </teleport>
     </template>
   </Popover>
 </template>
@@ -236,11 +170,6 @@
     position: static;
     padding: 10px;
     margin: -10px;
-
-    &:hover .svg-inline--fa {
-      color: $fallback--text;
-      color: var(--text, $fallback--text);
-    }
   }
 }
 </style>
